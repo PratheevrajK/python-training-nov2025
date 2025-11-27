53dd2909d7c03e9cac0f50fc99357f8c17e47361
PratheevrajK <pratheevraj002@gmail.com>
2025-11-27 03:55:33 +0000

    Lab 3

--- lab3.py
+++ lab3.py
@@ -2,6 +2,13 @@
 # Objective:
 # Practice type conversion and input/output formatting for a real-world scenario.
 # Problem Statement:
+METER_TO_FEET = 3.28084
+
 # Ask the user to enter a length in meters.
+meter_value = int(input('Enter length in meters: '))
+
 # Convert meters to feet (1 meter = 3.28084 feet).
+feet_value = METER_TO_FEET * meter_value
+
 # Display both values formatted to 2 decimal places.
+print(f"{meter_value:.2f} meter is {feet_value:.2f} feet")
